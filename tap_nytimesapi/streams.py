1bfb385bc4ff09fde4ab2951123dc4856947c9d6
MubaraqSani <mubaraq.sani@e4email.net>
2022-05-11 15:05:31 +0100

    Initial Setup

---
+++ tap_nytimesapi/streams.py
@@ -0,0 +1,31 @@
+"""Stream type classes for tap-nytimesapi."""
+
+from pathlib import Path
+from typing import Iterable
+import requests
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+
+
+class nytimesAPIStream(RESTStream):
+    """nytimesAPI stream class."""
+
+    @property
+    def url_base(self) -> str:
+    #     """Return the API URL root, configurable via tap settings."""
+        url_base = "https://api.nytimes.com/svc/search/v2"
+
+        api_url = f"{url_base}/{self.config['section']}/.json?api-key={self.config['api-key']}"
+        
+        return api_url
+
+    records_jsonpath = "$[*]"  # Or override `parse_response`.
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+            """Parse the response and return an iterator of result rows."""
+            
+            query_url = self.url_base()
+            r = requests.get(query_url)
+            
+            yield from extract_jsonpath(self.records_jsonpath, input=r.json())
---
+++ tap_nytimesapi/tap.py
@@ -0,0 +1,45 @@
+"""nytimesAPI tap class."""
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.join('..')))
+from typing import List
+
+from singer_sdk import Tap, Stream
+from singer_sdk.typing import (
+    PropertiesList,
+    Property,
+    StringType,
+    DateTimeType)
+
+
+from tap_nytimesapi.streams import (
+    nytimesAPIStream,
+)
+
+STREAM_TYPES = [
+    nytimesAPIStream,
+]
+
+
+class TapNyTimesAPI(Tap):
+    """nytimesAPI tap class."""
+    name = "tap-nytimesapi"
+
+    config_jsonschema = PropertiesList(
+        Property("api_key", StringType, required=True,
+            description="The token to authenticate against the API service"
+        ),
+        Property("section", StringType, required=True,
+            description="The section of the API to query"
+        ),
+        Property(
+            "start_date", DateTimeType,
+            description="The earliest record date to sync"
+        ),
+    ).to_dict()
+    
+    def discover_streams(self) -> List[Stream]:
+        """Return a list of discovered streams."""
+        return [stream_class(tap=self) for stream_class in STREAM_TYPES]
+
+cli = TapNyTimesAPI.cli
